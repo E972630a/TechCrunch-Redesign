fc73d232063cc18b169b7f0b401365010f214111
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-06-28 21:27:27 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 import Article1 from '../src/Components/Articles/Article1';
 import Article2 from '../src/Components/Articles/Article2'
 import ArticleHome from '../src/Components/Articles/ArticleHome';
+import{Route} from 'react-router-dom'
 
 
 function App() {
@@ -25,13 +26,13 @@ function App() {
       </Switch>
       <Footer />
     </BrowserRouter>
-
     <div className="App">
+     
       <Route exact path='/article1' component={Article1}/>
       <Route exact path='/article2' component={Article2}/>
       <Route exact path='/' component={ArticleHome}/>
+     
     </div>
-    
   );
 }
 
